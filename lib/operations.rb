28c72b2833e2599ba8531730e01ec1523f900a5e
Jashema Panapa <jcpanapa@gmail.com>
2020-03-26 22:52:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,18 @@
+def unsafe?(speed)
+  if speed < 40
+    puts true 
+  if speed > 60
+    puts true
+  else speed
+  puts false
+  end
+end
 
+
+def not_safe?(speed)
+	speed > 60 || speed < 40 ? true : false
+	
+end
+	
 
 
